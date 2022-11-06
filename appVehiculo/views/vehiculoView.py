33cf6a078b979dad43bd5362119147c41ac7db67
Miguel Angel Garcerant Amador <miguelgarcerant@gmail.com>
2022-11-06 17:32:10 -0500

    configurando .gitignore

--- appVehiculo/views/vehiculoView.py
+++ appVehiculo/views/vehiculoView.py
@@ -1,18 +1,16 @@
 from django.http import HttpResponse
-
-from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
-from rest_framework import status
+from rest_framework.response import Response
 
 from appVehiculo.models.vehiculo import Vehiculo
 from appVehiculo.serializers.vehiculoSerializer import VehiculoSerializer
 
 
-@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+@api_view(["GET", "POST", "PUT", "DELETE"])
 def gestionVehiculo(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         datos_vehiculo = Vehiculo.objects.all()
-        serializer_vehiculo = VehiculoSerializer(
-            datos_vehiculo, many=True)
+        serializer_vehiculo = VehiculoSerializer(datos_vehiculo, many=True)
         return Response(serializer_vehiculo.data)
